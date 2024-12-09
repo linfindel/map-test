2e17b0d7880b5ea47d4b64404684511677f7af8b
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-12-09 15:17:25 +0000

    Get tags from OSM API

--- map.js
+++ map.js
@@ -19,8 +19,46 @@ map.on('mousemove', (e) => {
       return displayFeat;
   });
 
-  const geometryType = displayFeatures[0]["geometry"];
+  let geometryType = displayFeatures[0]["geometry"].type;
+  let id = displayFeatures[0]["id"]
+
+  if (geometryType == "Point") {
+    geometryType = "node";
+  }
+
+  else if (geometryType == "LineString" || "Polygon" || "MultiLineString") {
+    geometryType = "way";
+  }
+
   console.log(geometryType);
+
+  fetch(`https://api.openstreetmap.org/api/0.6/${geometryType}/${id}/full.json`)
+  .then((response) => {
+    if (!response.ok) {
+        throw new Error
+            (`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    let elements = data.elements;
+    let tags;
+    elements.forEach(element => {
+      if (element.tags) {
+        tags = element.tags;
+        console.log(tags);
+        if (tags.name) {
+          document.getElementById("name").innerText = tags.name;
+        }
+
+        else {
+          document.getElementById("name").innerText = "";
+        }
+      }
+    });
+  })
+  .catch((error) => console.error("Unable to fetch data:", error));
+  
 
   document.getElementById('features').innerHTML = JSON.stringify(
       displayFeatures,
